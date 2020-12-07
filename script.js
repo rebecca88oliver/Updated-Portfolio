b45482b12d2dfa4453611586c966456d69862a21
Rebecca88 <rebecca88oliver@gmail.com>
2020-12-07 18:14:26 -0500

    updating

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-
-
-
-
+//Shows contact information on homepage
 function getContactInfo(){
-
-
 document.getElementById("contactInfo").innerHTML = "Phone: (860) 989-7261 <br> rebecca88oliver@gmail.com ";
-
-
 ;}
